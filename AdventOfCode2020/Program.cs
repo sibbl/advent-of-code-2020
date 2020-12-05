75d27753f83102b6d6d56b1b8f242e3b56a744c9
Sebastian Schubotz <aoc2020@sibbl.net>
2020-12-05 12:25:59 +0100

    Use top level statements in Program.cs

--- AdventOfCode2020/Program.cs
+++ AdventOfCode2020/Program.cs
@@ -6,82 +6,73 @@ using AdventOfCode2020.Day03;
 using AdventOfCode2020.Day04;
 using AdventOfCode2020.Day05;
 
-namespace AdventOfCode2020
+await DayOneProblemOne();
+await DayOneProblemTwo();
+await DayTwoProblemOne();
+await DayTwoProblemTwo();
+await DayThreeProblemOne();
+await DayThreeProblemTwo();
+await DayFourProblemOne();
+await DayFourProblemTwo();
+await DayFiveProblemOne();
+await DayFiveProblemTwo();
+
+static async Task DayOneProblemOne()
 {
-    public class Program
-    {
-        static async Task Main(string[] args)
-        {
-            await DayOneProblemOne();
-            await DayOneProblemTwo();
-            await DayTwoProblemOne();
-            await DayTwoProblemTwo();
-            await DayThreeProblemOne();
-            await DayThreeProblemTwo();
-            await DayFourProblemOne();
-            await DayFourProblemTwo();
-            await DayFiveProblemOne();
-            await DayFiveProblemTwo();
-        }
+    var output = await Solution01.ProblemOneAsync();
+    Console.WriteLine($"Day 1, Problem 1: {output}");
+}
 
-        private static async Task DayOneProblemOne()
-        {
-            var output = await Solution01.ProblemOneAsync();
-            Console.WriteLine($"Day 1, Problem 1: {output}");
-        }
+static async Task DayOneProblemTwo()
+{
+    var output = await Solution01.ProblemTwoAsync();
+    Console.WriteLine($"Day 1, Problem 2: {output}");
+}
 
-        private static async Task DayOneProblemTwo()
-        {
-            var output = await Solution01.ProblemTwoAsync();
-            Console.WriteLine($"Day 1, Problem 2: {output}");
-        }
+static async Task DayTwoProblemOne()
+{
+    var output = await Solution02.ProblemOneAsync();
+    Console.WriteLine($"Day 2, Problem 1: {output}");
+}
 
-        private static async Task DayTwoProblemOne()
-        {
-            var output = await Solution02.ProblemOneAsync();
-            Console.WriteLine($"Day 2, Problem 1: {output}");
-        }
+static async Task DayTwoProblemTwo()
+{
+    var output = await Solution02.ProblemTwoAsync();
+    Console.WriteLine($"Day 2, Problem 2: {output}");
+}
 
-        private static async Task DayTwoProblemTwo()
-        {
-            var output = await Solution02.ProblemTwoAsync();
-            Console.WriteLine($"Day 2, Problem 2: {output}");
-        }
+static async Task DayThreeProblemOne()
+{
+    var output = await Solution03.ProblemOneAsync();
+    Console.WriteLine($"Day 3, Problem 1: {output}");
+}
 
-        private static async Task DayThreeProblemOne()
-        {
-            var output = await Solution03.ProblemOneAsync();
-            Console.WriteLine($"Day 3, Problem 1: {output}");
-        }
+static async Task DayThreeProblemTwo()
+{
+    var output = await Solution03.ProblemTwoAsync();
+    Console.WriteLine($"Day 3, Problem 2: {output}");
+}
 
-        private static async Task DayThreeProblemTwo()
-        {
-            var output = await Solution03.ProblemTwoAsync();
-            Console.WriteLine($"Day 3, Problem 2: {output}");
-        }
+static async Task DayFourProblemOne()
+{
+    var output = await Solution04.ProblemOneAsync();
+    Console.WriteLine($"Day 4, Problem 1: {output}");
+}
 
-        private static async Task DayFourProblemOne()
-        {
-            var output = await Solution04.ProblemOneAsync();
-            Console.WriteLine($"Day 4, Problem 1: {output}");
-        }
+static async Task DayFourProblemTwo()
+{
+    var output = await Solution04.ProblemTwoAsync();
+    Console.WriteLine($"Day 4, Problem 2: {output}");
+}
 
-        private static async Task DayFourProblemTwo()
-        {
-            var output = await Solution04.ProblemTwoAsync();
-            Console.WriteLine($"Day 4, Problem 2: {output}");
-        }
+static async Task DayFiveProblemOne()
+{
+    var output = await Solution05.ProblemOneAsync();
+    Console.WriteLine($"Day 5, Problem 1: {output}");
+}
 
-        private static async Task DayFiveProblemOne()
-        {
-            var output = await Solution05.ProblemOneAsync();
-            Console.WriteLine($"Day 5, Problem 1: {output}");
-        }
-
-        private static async Task DayFiveProblemTwo()
-        {
-            var output = await Solution05.ProblemTwoAsync();
-            Console.WriteLine($"Day 5, Problem 2: {output}");
-        }
-    }
+static async Task DayFiveProblemTwo()
+{
+    var output = await Solution05.ProblemTwoAsync();
+    Console.WriteLine($"Day 5, Problem 2: {output}");
 }
